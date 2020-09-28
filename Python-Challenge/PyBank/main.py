f6c0a9149fac2be30c9edde2aecfa5ad686553a7
Serafin <serafinjuarez@yahoo.com>
2020-09-27 17:03:12 -0700

    Starting the python file

---
+++ Python-Challenge/PyBank/main.py
@@ -0,0 +1,14 @@
+
+
+print("Financial Analysis")
+print("-------------------------------------------------------")
+
+# Total number of months included in dataset
+
+# The net total amount of "Profit/Losses" over the entire period
+
+# The average of the changes in "profit/Losses" over the entire period
+
+# The greatest increase in profits (date and amount) over the entire period
+
+# The greatest decrease in losses (date and amount) over hte entire period
