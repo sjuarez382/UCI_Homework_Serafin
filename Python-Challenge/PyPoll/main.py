e048fcefab626814c45056bcc260b3363fc5225a
Serafin <serafinjuarez@yahoo.com>
2020-09-30 12:26:57 -0700

    Adding variables from pybank

--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -3,3 +3,9 @@ import csv
 
 # path to collect data 
 pypoll_csv = os.path.join('..', 'csv_files', 'election_data.csv')
+
+#declaring all my variables
+
+total_months = []
+monthly_change = []
+net_total = []
