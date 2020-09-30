bbe6b5945b052759c3cba7976afbcc7111e9c90c
Serafin <serafinjuarez@yahoo.com>
2020-09-30 12:25:52 -0700

    Adding csv name

--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -2,4 +2,4 @@ import os
 import csv 
 
 # path to collect data 
-pybank_csv = os.path.join('..', 'csv_files', 'election_data.csv')
+pypoll_csv = os.path.join('..', 'csv_files', 'election_data.csv')
