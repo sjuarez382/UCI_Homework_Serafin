19a9fa374f419ac08188256c508c1fa52dce3be9
Serafin <serafinjuarez@yahoo.com>
2020-09-29 20:36:09 -0700

    moving print statements

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -20,9 +20,7 @@ with open(pybank_csv) as csvfile:
     
     csv_header = next(csvreader)
 
-# printing
-    print("Financial Analysis")
-    print("-------------------------------------------------------")
+
 
 # Total number of months included in dataset
     for row in csv.reader(csvfile):
@@ -30,25 +28,33 @@ with open(pybank_csv) as csvfile:
 
 # The net total amount of "Profit/Losses" over the entire period
         net_total.append(int(row[1]))
-       
+
+# printing
+    print("Financial Analysis")
+    print("-------------------------------------------------------")       
 
 # The average of the changes in "profit/Losses" over the entire period
     for i in range(len(net_total)-1):
             monthly_change.append(net_total[i + 1] - net_total[i])
-            print(f'{len(total_months)}')
-            print(f'{sum(net_total)}')    
+              
 
 # The greatest increase in profits (date and amount) over the entire period
 greatest_increase_profit = max(monthly_change)
 greatest_decrease_profit = min(monthly_change)
 monthly_increase = monthly_change.index(max(monthly_change)) + 1
 Monthly_decrease = monthly_change.index(min(monthly_change)) + 1
-print(f'{round(sum(monthly_change)/len(monthly_change),2)}')
-
         
 
 # The greatest decrease in losses (date and amount) over hte entire period
 greatest_increase_dt = total_months[monthly_change.index(greatest_increase_profit)]
 greatest_decrease_dt = total_months[monthly_change.index(greatest_decrease_profit)]
+
+
+
+# print stuff
+print(f'{round(sum(monthly_change)/len(monthly_change),2)}')
 print(f'{total_months[monthly_increase]} (${(str(greatest_increase_profit))}')
 print(f'{total_months[Monthly_decrease]} (${(str(greatest_decrease_profit))}')
+
+print(f'{len(total_months)}')
+print(f'{sum(net_total)}')  
