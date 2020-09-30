77a90c7a3e18ef3962feaf5fd4051590cf5a597f
Serafin <serafinjuarez@yahoo.com>
2020-09-29 20:25:12 -0700

    max not working

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -44,9 +44,11 @@ greatest_increase_profit = max(monthly_change)
 greatest_decrease_profit = min(monthly_change)
 monthly_increase = monthly_change.index(max(monthly_change)) + 1
 Monthly_decrease = monthly_change.index(min(monthly_change)) + 1
+print(f'{round(sum(monthly_change)/len(monthly_change),2)}')
 
         
 
 # The greatest decrease in losses (date and amount) over hte entire period
-        #greatest_increase_dt = date[monthly_change.index(greatest_increase_profit)]
-        #greatest_decrease_dt = date[monthly_change.index(greatest_decrease_profit)]
+greatest_increase_dt = total_months[monthly_change.index(greatest_increase_profit)]
+greatest_decrease_dt = total_months[monthly_change.index(greatest_decrease_profit)]
+print(f'{total_months[monthly_increase]} (${(str(greatest_increase_profit))}')
