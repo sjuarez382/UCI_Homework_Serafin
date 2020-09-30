dc64a09cab6edebfcb87490c731228ce0686370b
Serafin <serafinjuarez@yahoo.com>
2020-09-30 16:16:52 -0700

    Final edit after removing header print from early test

--- Python-Challenge/PyPoll/main.py
+++ Python-Challenge/PyPoll/main.py
@@ -16,7 +16,6 @@ with open(pypoll_csv) as csvfile:
 #checking header
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"Header:{csv_header}")
 
 #total votes
     for row in csv.reader(csvfile):
