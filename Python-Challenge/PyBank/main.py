db8440575f10380168e29eb4571b7475ebda7f57
Serafin <serafinjuarez@yahoo.com>
2020-09-29 20:38:14 -0700

    rearranging print statements again

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -50,10 +50,11 @@ greatest_decrease_dt = total_months[monthly_change.index(greatest_decrease_profi
 
 # printing
 print("Financial Analysis")
-print("-------------------------------------------------------")       
+print("-------------------------------------------------------")
+print(f'{len(total_months)}')
+print(f'{sum(net_total)}')
 print(f'{round(sum(monthly_change)/len(monthly_change),2)}')
 print(f'{total_months[monthly_increase]} (${(str(greatest_increase_profit))}')
 print(f'{total_months[Monthly_decrease]} (${(str(greatest_decrease_profit))}')
 
-print(f'{len(total_months)}')
-print(f'{sum(net_total)}')  
+  
