653026ad01dc806af531c8af942580bad23918dd
Serafin <serafinjuarez@yahoo.com>
2020-10-18 16:54:36 -0700

    Starting

--- Python-API-Homework_Whats_the_Weather_like/api_keys.py
+++ Python-API-Homework_Whats_the_Weather_like/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "743eab4b88f112303648e5c13eb272bf"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDQ5beFz0j2IUNfsWtilN00oUoU8qF56uo"
