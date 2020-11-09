ad634131c0407fc84e989e9c48c24eb8383bad63
Serafin <serafinjuarez@yahoo.com>
2020-11-08 16:20:01 -0800

    Adding DBD SQL Querie removing quotes

--- sql-challenge/EmployeeSQL/Employee_DBD_Querie.sql
+++ sql-challenge/EmployeeSQL/Employee_DBD_Querie.sql
@@ -2,78 +2,78 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
+CREATE TABLE departments (
+    dept_no varchar   NOT NULL,
+    dept_name varchar   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
      )
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
-    "from_date" date   NOT NULL,
-    "to_date" date   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no","dept_no"
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL,
+    dept_no varchar   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no,dept_no
      )
 );
 
-CREATE TABLE "dept_manager" (
-    "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "emp_no"
+CREATE TABLE dept_manager (
+    emp_no int   NOT NULL,
+    dept_no varchar   NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        emp_no
      )
 );
 
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "gender" varchar   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
+CREATE TABLE employees (
+    emp_no int   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    gender varchar   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
      )
 );
 
-CREATE TABLE "salaries" (
-    "emp_no" integer   NOT NULL,
-    "salary" integer   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
+CREATE TABLE salaries (
+    emp_no integer   NOT NULL,
+    salary integer   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
      )
 );
 
-CREATE TABLE "titles" (
-    "emp_no" int   NOT NULL,
-    "title" varchar   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "emp_no"
+CREATE TABLE titles (
+    emp_no int   NOT NULL,
+    title varchar   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        emp_no
      )
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_manager" ("emp_no");
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES dept_manager (emp_no);
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_emp" ("emp_no");
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES dept_emp (emp_no);
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
 
