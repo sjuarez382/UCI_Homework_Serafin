791aaef27ea0e7472e1e8ed7e6725e88573787d8
Serafin <serafinjuarez@yahoo.com>
2020-11-09 17:53:05 -0800

    completed load of tables and data

--- sql-challenge/EmployeeSQL/Employee_DBD_Querie.sql
+++ sql-challenge/EmployeeSQL/Employee_DBD_Querie.sql
@@ -1,11 +1,10 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
---drop table departments cascade --"used to drop tables and everything tied to it"
-SELECT * FROM titles
-
+--drop table titles cascade --"used to drop tables and everything tied to it"
+----------------------------------------------------------------------------------------------------
 CREATE TABLE departments (
-    dept_no varchar   NOT NULL,
-    dept_name varchar   NOT NULL,
+    dept_no varchar(4)   NOT NULL,
+    dept_name varchar(255)   NOT NULL,
     CONSTRAINT pk_departments PRIMARY KEY (
         dept_no
      )
@@ -13,21 +12,21 @@ CREATE TABLE departments (
 
 CREATE TABLE dept_emp (
     emp_no int   NOT NULL,
-    dept_no varchar   NOT NULL
+    dept_no varchar(4)   NOT NULL
 );
 
 CREATE TABLE dept_manager (
-    dept_no varchar   NOT NULL,
+    dept_no varchar(4)   NOT NULL,
     emp_no int   NOT NULL
 );
 
 CREATE TABLE employees (
     emp_no int   NOT NULL,
-    title_id varchar   NOT NULL,
+    title_id varchar(10)   NOT NULL,
     birth_date date   NOT NULL,
-    first_name varchar   NOT NULL,
-    last_name varchar   NOT NULL,
-    sex varchar   NOT NULL,
+    first_name varchar(255)   NOT NULL,
+    last_name varchar(255)   NOT NULL,
+    sex char   NOT NULL,
     hire_date date   NOT NULL,
     CONSTRAINT pk_employees PRIMARY KEY (
         emp_no
@@ -35,17 +34,23 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE salaries (
-    emp_no integer   NOT NULL,
-    salary integer   NOT NULL
+    emp_no int   NOT NULL,
+    salary int   NOT NULL
 );
 
 CREATE TABLE titles (
-    title_id varchar   NOT NULL,
-    title varchar   NOT NULL,
+    title_id varchar(10)   NOT NULL,
+    title varchar(255)   NOT NULL,
     CONSTRAINT pk_titles PRIMARY KEY (
         title_id
      )
 );
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
 ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
 REFERENCES departments (dept_no);
@@ -53,12 +58,13 @@ REFERENCES departments (dept_no);
 ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
 REFERENCES employees (emp_no);
 
-ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_no FOREIGN KEY(emp_no)
-REFERENCES dept_emp (emp_no);
+ALTER TABLE employees ADD CONSTRAINT fk_employees_title_id FOREIGN KEY(title_id)
+REFERENCES titles (title_id);
 
 ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
 REFERENCES employees (emp_no);
 
-ALTER TABLE titles ADD CONSTRAINT fk_titles_title_id FOREIGN KEY(title_id)
-REFERENCES employees (title_id);
 
+
+-----------------------------------------------------------------------------------------------------
+SELECT * FROM titles
--- sql-challenge/EmployeeSQL/QuickDBD_code.md
+++ sql-challenge/EmployeeSQL/QuickDBD_code.md
@@ -2,39 +2,39 @@
 
 departments
 -
-dept_no PK varchar
-dept_name varchar
+dept_no PK varchar(4)
+dept_name varchar(255)
 
 dept_emp
 -
-emp_no int
-dept_no varchar
+emp_no int FK - employees.emp_no
+dept_no varchar(4) FK - departments.dept_no
 
 
 dept_manager
 -
-dept_no varchar FK >- departments.dept_no
+dept_no varchar(4) FK - departments.dept_no
 emp_no int FK - employees.emp_no
 
 
 
 employees
 -
-emp_no PK int FK -< dept_emp.emp_no
-title_id varchar
+emp_no PK int
+title_id varchar(10) FK - titles.title_id
 birth_date date
-first_name varchar
-last_name varchar
-sex varchar
+first_name varchar(255)
+last_name varchar(255)
+sex char
 hire_date date
 
 salaries
 -
-emp_no integer FK - employees.emp_no
-salary integer
+emp_no int FK - employees.emp_no
+salary int
 
 
 titles
 -
-title_id varchar PK FK >- employees.title_id
-title varchar
+title_id varchar(10) PK
+title varchar(255)
