3469035dfaa51424bc5ef766dc467de7716887fa
Serafin <serafinjuarez@yahoo.com>
2020-11-09 18:34:13 -0800

    End of schemata

--- sql-challenge/EmployeeSQL/Employee_DBD_Querie.sql
+++ sql-challenge/EmployeeSQL/Employee_DBD_Querie.sql
@@ -67,4 +67,9 @@ REFERENCES employees (emp_no);
 
 
 -----------------------------------------------------------------------------------------------------
+SELECT * FROM departments
+SELECT * FROM dept_emp
+SELECT * FROM dept_manager
+SELECT * FROM employees
+SELECT * FROM salaries
 SELECT * FROM titles
