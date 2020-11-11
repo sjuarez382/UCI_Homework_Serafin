63ce4d74ad0b284d91d82987faaeac0bb83be46d
Serafin <serafinjuarez@yahoo.com>
2020-11-10 16:33:31 -0800

    hire date query re ordered

--- sql-challenge/EmployeeSQL/EmployeeQueries.sql
+++ sql-challenge/EmployeeSQL/EmployeeQueries.sql
@@ -3,11 +3,13 @@ select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 from employees as e
 left join salaries as s
 on e.emp_no = s.emp_no
+order by salary desc
 
 --List first name, last name, and hire date for employees who were hired in 1986.
 select emp_no, last_name, first_name, hire_date
 from employees
 where hire_date between '1986/1/1' and '1986/12/31'
+order by emp_no
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
