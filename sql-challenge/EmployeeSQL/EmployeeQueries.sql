793fa30f13e2100d3913e8d6bbf421ee82cda99b
Serafin <serafinjuarez@yahoo.com>
2020-11-10 16:24:50 -0800

    employee query

---
+++ sql-challenge/EmployeeSQL/EmployeeQueries.sql
@@ -0,0 +1,25 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees as e
+left join salaries as s
+on e.emp_no = s.emp_no
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
