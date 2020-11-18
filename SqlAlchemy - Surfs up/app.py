b9c49f684ac15efc85412bbb06e34491d5aea14c
Serafin <serafinjuarez@yahoo.com>
2020-11-17 18:53:10 -0800

    adding notes

--- SqlAlchemy - Surfs up/app.py
+++ SqlAlchemy - Surfs up/app.py
@@ -12,19 +12,23 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
 
-#
+#Return a JSON list of stations from the dataset.
 @app.route("/api/v1.0/stations")
 def stations():
 
-#
+#Query the dates and temperature observations of the most active station for the last year of data.
+#Return a JSON list of temperature observations (TOBS) for the previous year.
 @app.route("/api/v1.0/tobs")
 def tobs():
 
-#
+
+#Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+
+#When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 @app.route("/api/v1.0/<start>")
 def start():
 
-#
+#When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
 @app.route("/api/v1.0/<start>/<end>")
 def end():
 
