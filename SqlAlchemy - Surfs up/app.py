16bdf6154f08b2f62a55ed16ee8007f9d306a4c2
Serafin <serafinjuarez@yahoo.com>
2020-11-17 18:49:21 -0800

    adding apps

--- SqlAlchemy - Surfs up/app.py
+++ SqlAlchemy - Surfs up/app.py
@@ -7,3 +7,42 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("Here are all of the available routes")
+
+#Convert the query results to a dictionary using date as the key and prcp as the value.
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+
+#
+@app.route("/api/v1.0/stations")
+def stations():
+
+#
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+#
+@app.route("/api/v1.0/<start>")
+def start():
+
+#
+@app.route("/api/v1.0/<start>/<end>")
+def end():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
