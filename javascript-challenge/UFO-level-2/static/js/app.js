8dd159e39602d91fc950413db65f0bb9695198a7
Serafin <serafinjuarez@yahoo.com>
2020-12-18 20:02:20 -0800

    trying out bonus

---
+++ javascript-challenge/UFO-level-2/static/js/app.js
@@ -0,0 +1,36 @@
+// from data.js
+var tableData = data;
+console.log(tableData);
+tbody = d3.select("tbody");
+
+
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("filter-btn");
+button.on("click", function() {
+    tbody.html("");
+    
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+        console.log(selections);
+        var row = tbody.append("tr");
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
