7962bddafe21f0a0c4e57c8b884078a43b998da7
Serafin <serafinjuarez@yahoo.com>
2020-12-19 11:03:48 -0800

    fixing some code

--- javascript-challenge/UFO-level-2/static/js/app.js
+++ javascript-challenge/UFO-level-2/static/js/app.js
@@ -1,36 +1,46 @@
 // from data.js
 var tableData = data;
-console.log(tableData);
-tbody = d3.select("tbody");
 
+tbody = d3.select("tbody")
 
-tableData.forEach(function(ufoSighting) {
-    console.log(ufoSighting);
-    var row = tbody.append("tr");
-    Object.entries(ufoSighting).forEach(function([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
+function displayData(ufoSighting){
+    tbody.text("")
+    ufoSighting.forEach(function(ufo_sighting){
+        new_tr = tbody.append("tr")
+        Object.entries(ufo_sighting).forEach(function([key, value]){
+            new_td = new_tr.append("td").text(value)
+        })
+    })
+}
+//displaying the data
+displayData(tableData)
 
-var button = d3.select("filter-btn");
-button.on("click", function() {
-    tbody.html("");
-    
-    var inputElement = d3.select("#input");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue || sighting.city === inputValue || sighting.state === inputValue || sighting.country === inputValue || sighting.shape === inputValue);
-    console.log(filteredData);
+// button!!
+var button = d3.select("#submit");
+submit.on("click", function(){
+    console.log("filter button test")
 
-    filteredData.forEach(function(selections) {
-        console.log(selections);
-        var row = tbody.append("tr");
-        Object.entries(selections).forEach(function([key, value]) {
-            console.log(key, value);
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });
+    d3.event.preventDefault();
+
+    var dateInput = d3.select("#datetime");
+    var cityInput = d3.select("#city");
+    var stateInput = d3.select("state");
+    var countryInput = d3.select("#country");
+    var shapeInput = d3.select("#shape");
+
+    console.log(dateInput.property("value"));
+    console.log(cityInput.property("value"));
+    console.log(stateInput.property("value"));
+    console.log(countryInput.property("value"));
+    console.log(shapeInput.property("value"));
+
+    var filtered = tableData.filter(ufo_sighting => {
+        return (ufo_sighting.datetime === dateInput.property("value") || !dateInput.property("value") ) &&
+        (ufo_sighting.city === cityInput.property("value") || !cityInput.property("value") ) &&
+        (ufo_sighting.state === stateInput.property("value") || !stateInput.property("value") ) &&
+        (ufo_sighting.country === countryInput.property("value") || !sountryInput.property("value") ) &&
+        (ufo_sighting.shape === shapeInput.property("value") || !shapeInput.property("value") ) &&
+
+    })
+    displayData(filtered);
 });
