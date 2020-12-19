039b002b06c087b3521cd4d602dd278c8bda8fc5
Serafin <serafinjuarez@yahoo.com>
2020-12-18 19:38:52 -0800

    completing code

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -12,3 +12,28 @@ tableData.forEach(ufodata => {
         cell.text(value);
     });
 });
+
+// Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
+
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    var date_input = d3.select("#datetime");
+    var input_value = date_input.properly("value");
+    console.log(input_value);
+    tbody.html("");
+
+    var filteredData = tableData.filter(data => data.datetime === input_value);
+    console.log(filteredData);
+
+    if (filteredData.length >= 1) {
+        filteredData.forEach(filtered_data => {
+            var row = tbody.append("tr");
+            Object.defineProperties(filtered_data).forEach(function([key, value]) {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
+    } else {
+        tbody.append("h5").text("Data not found for this selection. Please retry with another date!");
+    }
+});
