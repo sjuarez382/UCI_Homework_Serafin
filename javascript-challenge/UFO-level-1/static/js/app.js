97c9d58ede1580db42de65959792710343c725f9
Serafin <serafinjuarez@yahoo.com>
2020-12-18 19:31:30 -0800

    starting code

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+tbody = d3.select("tbody");
+
+// Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
+tableData.forEach(ufodata => {
+    var row = tbody.append("tr");
+    Object.defineProperties(ufodata).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
