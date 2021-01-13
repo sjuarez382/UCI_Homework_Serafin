54c97536da06530b846daabf33d7279e150a5ea6
Serafin <serafinjuarez@yahoo.com>
2021-01-12 19:00:13 -0800

    loading csv

--- D3-Challenge/assets/js/app.js
+++ D3-Challenge/assets/js/app.js
@@ -11,3 +11,17 @@ var margin = {
 
 //dimensions for chart
 
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+//starting d3 code for body and setting dimensions
+var svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+var chartGroup = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//loading csv
