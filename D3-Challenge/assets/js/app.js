06d0422d8b7ba1d38ecc0ea9019eeb162ff5090f
Serafin <serafinjuarez@yahoo.com>
2021-01-12 18:54:38 -0800

    starting code

--- D3-Challenge/assets/js/app.js
+++ D3-Challenge/assets/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+// var svg dimensions
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 60,
+    right: 60,
+    bottom: 60,
+    left: 80
+  };
+
+//dimensions for chart
+
