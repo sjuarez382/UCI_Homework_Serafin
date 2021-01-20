23a0aed4f74d6e7109e8ac452c7f6395c80a6610
Serafin <serafinjuarez@yahoo.com>
2021-01-19 18:01:23 -0800

    adding layer outdoors

--- leaflet-challenge/static/js/logic.js
+++ leaflet-challenge/static/js/logic.js
@@ -97,3 +97,14 @@ function createMap(earthquakeCircles) {
           accessToken: API_KEY,
         }
       );
+
+    var outdoors = L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox.outdoors",
+      accessToken: API_KEY,
+    }
+  );  
