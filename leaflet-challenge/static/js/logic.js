3ac03c9bc4d8f13b4db2ae6f4565afdf0a421d13
Serafin <serafinjuarez@yahoo.com>
2021-01-19 18:00:37 -0800

    adding layer street

--- leaflet-challenge/static/js/logic.js
+++ leaflet-challenge/static/js/logic.js
@@ -72,4 +72,28 @@ function createFeatures(earthquakeData) {
 
     //earthquake layer
     createImageBitmap(earthquakeCircles);
-  }
+}
+
+function createMap(earthquakeCircles) {
+    // Create tiles
+    var satellite = L.tileLayer(
+      "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox.streets-satellite",
+        accessToken: API_KEY,
+      }
+    );
+
+    var streets = L.tileLayer(
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox.streets",
+          accessToken: API_KEY,
+        }
+      );
